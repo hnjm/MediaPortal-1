06f6451013fe8f60b121b6bc404f77429f7b8ee7
Azzuro <Azzuro@users.noreply.github.com>
2020-05-04 22:17:16 +0200

    fix

--- Build/Build_BD_Java.bat
+++ Build/Build_BD_Java.bat
@@ -1,7 +1,12 @@
-if not defined GIT_ROOT call .\BuildInit.bat
+if not defined GIT_ROOT (
+call .\BuildInit.bat
+)
 
 if not defined ant_home set ant_home=%NugetPackages%\ANT.1.10.7\tools
 
-pushd %GIT_ROOT%\libbluray\src\libbluray\bdj
-call %ant_home%\bin\ant -Dsrc_awt=:java-j2se
-popd
+call %ant_home%\bin\ant -f %LibblurayJAR% -Dsrc_awt=:java-j2se
+
+rem previous command backup
+rem pushd %GIT_ROOT%\src\libbluray\bdj
+rem call %ant_home%\bin\ant -Dsrc_awt=:java-j2se
+rem popd
